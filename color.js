8f1c5b4b7f2eb5ee2d0566bd3fc238b4124ba302
luckyjek <70943889+luckyjek@users.noreply.github.com>
2021-06-23 22:30:16 +0900

    add_var_prompt

---
+++ color.js
@@ -0,0 +1,35 @@
+var currentYear = 2021;
+var birthYear;
+var age;
+birthYear = prompt("태어난 연도를 입력하세요. (YYYY)", "");
+age = currentYear - birthYear + 1;
+document.write(currentYear + "년 현재<br>");
+document.write(birthYear+"년에 태어난 사람의 나이는"+age+"세 입니다.");
+
+
+// var now = new Date();
+// var display = now.toLocalTimeString();
+// document.write("현재 시간은" + display);
+
+// var name=prompt("이름을 입력하세요.");
+// console.log(name+"님, 환영합니다.");
+
+  // var name = prompt("이름을 입력하세요."); 
+  // document.write("<b><big>"+ name +"</big></b>,님 환영합니다.");
+
+//document.write("<h1>어서오세요</h1>");
+
+//var name = prompt("이름을 입력하세요.");
+
+//var name = prompt("이름을 입력하세요.", "아무개");
+
+//var reply = confirm(" 정말 배경 이미지를 바꾸겠습니까?");
+
+//alert("안녕하세요?")
+
+
+// var heading = document.querySelector('#heading');
+// heading.onclick = function()
+// {
+//   heading.style.color = "red";
+// }
