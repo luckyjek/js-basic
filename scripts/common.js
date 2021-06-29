547f3428b8fbe9c6439a43cb5883cd379bf52836
luckyjek <thxjek@gmail.com>
2021-06-29 16:19:28 +0900

    Update common.js

--- scripts/common.js
+++ scripts/common.js
@@ -1,3 +1,16 @@
+//별도로 자바스크립트 파일을 사용하면 혼자만쓰는것이아니므로
+//아래와같은 주석을 잘 달아줘야된다. 
+// /하고 **을 하고 enter하면된다. 
+
+
+//대규모로 사용할때 ->  Type스크립트를 꼭 써준다
+//규약을 맺고, 사용하는것이다.
+/**
+ * //함수가 어떤 기능을 하는지 
+ * @param {*} userList 
+ * @param {*} keyAge 
+ * @returns 
+ */
 function getAgeGroup(userList, keyAge) {
     let userAgeGroup = {};
 
@@ -16,7 +29,12 @@ function getAgeGroup(userList, keyAge) {
 
     return userAgeGroup;
 }
-
+/**
+ * 
+ * @param {*} userList 
+ * @param {*} keyAge 
+ * @returns 
+ */
 function getAgeGroup2(userList, keyAge) {
     let myMap = new Map();
 
@@ -36,12 +54,20 @@ function getAgeGroup2(userList, keyAge) {
     return myMap;
 }
 
-
+/**
+ * 최소값과 최대값을 파라미터로 받고 그 사이의 무작위(random) 정수 값을 반환하는 함수
+ * @param {Number} min 
+ * @param {Number} max 
+ * @returns {Number} min과 max사이의 랜덤 정수 값 
+ */
 function getRandomInterger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-
+/**
+ * 가위바위보 함수
+ * @param {*} userRsp 
+ * @returns 
+ */
 function rspPlayer(userRsp) {
     const rsp = ["가위", "바위", "보"];
     const playerRsp = rsp[getRandomInterger(0, 2)];
